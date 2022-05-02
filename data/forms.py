b8b0c00b83bc21d0e07ade2eab29178f025102b9
Amir <g.amir2004@gmail.com>
2022-05-02 10:39:42 +0300

    Fix auth, add profile, add quiz_create base

--- data/forms.py
+++ data/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
-from wtforms.fields.html5 import EmailField
+from wtforms.fields import EmailField
 from wtforms.validators import DataRequired
 
 
@@ -10,14 +10,24 @@ class RegisterForm(FlaskForm):
     password = PasswordField('Password', validators=[DataRequired()])
     password_again = PasswordField('Repeat password',
                                    validators=[DataRequired()])
-    last_name = StringField('Surname', validators=[DataRequired()])
-    first_name = StringField('Name', validators=[DataRequired()])
+    last_name = StringField('Last_name', validators=[DataRequired()])
+    first_name = StringField('First_name', validators=[DataRequired()])
     submit = SubmitField('Sign up')
 
 
 class LoginForm(FlaskForm):
     username_or_email = StringField('Username or email',
-                                   validators=[DataRequired()])
+                                    validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
     remember_me = BooleanField('Запомнить меня')
     submit = SubmitField('Log in')
+
+
+class EditForm(FlaskForm):
+    username = StringField('Username')
+    email = EmailField('Enter your email')
+    password = PasswordField('Password')
+    password_again = PasswordField('Repeat password')
+    last_name = StringField('Last_name')
+    first_name = StringField('First_name')
+    submit = SubmitField('Edit')
--- views.py
+++ views.py
@@ -1,10 +1,11 @@
 from flask import Flask, app, render_template, redirect
-from flask_login import LoginManager, login_user, login_required, logout_user
+from flask_login import LoginManager, login_user, login_required, logout_user, \
+    current_user
 from flask_restful import Api
 
 from data import db_session
 from data import quiz_resources
-from data.forms import LoginForm, RegisterForm
+from data.forms import LoginForm, RegisterForm, EditForm
 from data.user_db import User
 
 app = Flask(__name__)
@@ -42,7 +43,10 @@ def login():
     if form.validate_on_submit():
         db_sess = db_session.create_session()
         user = db_sess.query(User).filter(
-            User.username_or_email == form.username_or_email.data).first()
+            User.username == form.username_or_email.data).first()
+        if not user:
+            user = db_sess.query(User).filter(
+                User.email == form.username_or_email.data).first()
         if user and user.check_password(form.password.data):
             login_user(user, remember=form.remember_me.data)
             return redirect("/")
@@ -75,7 +79,7 @@ def register():
         user.set_password(form.password.data)
         db_sess.add(user)
         db_sess.commit()
-        return redirect('/login')
+        return redirect('/auth/login')
     return render_template('auth/register.html', **context)
 
 
@@ -86,29 +90,46 @@ def logout():
     return redirect("/")
 
 
-@app.route('/auth/profile')
+@app.route('/auth/profile', methods=['GET', 'POST'])
 @login_required
 def profile():
+    form = EditForm()
     context = {
-        'title': 'USER_NAME',
+        'title': str(current_user.username),
+        'form': form,
     }
+    if form.validate_on_submit():
+        db_sess = db_session.create_session()
+        user = db_sess.query(User).filter(User.id == current_user.id).first()
+
+        if form.password.data != form.password_again.data:
+            context['message'] = "Пароли не совпадают"
+            return render_template('auth/profile.html', **context)
+        if db_sess.query(User).filter(User.email == form.email.data).first():
+            context['message'] = "Такой email уже занят"
+            return render_template('auth/profile.html', **context)
+        if db_sess.query(User).filter(
+                User.username == form.username.data).first():
+            context['message'] = 'Такой username уже занят'
+            return render_template('auth/profile.html', **context)
+
+        if user:
+            user.username = form.username.data
+            user.email = form.email.data
+            user.last_name = form.last_name.data
+            user.first_name = form.first_name.data
+            user.set_password(form.password.data)
+            db_sess.commit()
+            return redirect('/auth/profile')
     return render_template('auth/profile.html', **context)
 
 
-@app.route('/quizzes/')
-def quizzes_list():
+@app.route('/quizzes/create')
+def quiz_create():
     context = {
-        'title': 'List of Quizzes',
+        'title': 'Create quiz'
     }
-    return render_template('quizzes/quizzes_lis.html', **context)
-
-
-@app.route('/quizzes/<int:pk>/')  # primary key (Первичный ключ)
-def quiz_info(pk):
-    context = {
-        'title': 'QUIZ_NAME',  # Replace with the name of quiz
-    }
-    return render_template('quizzes/quiz_info.html', **context)
+    render_template('quizzes/quiz_create.html')
 
 
 @app.route('/quizzes/<int:pk>/delete')
@@ -125,6 +146,22 @@ def quiz_edit(pk):
         'title': 'QUIZ_NAME + _edit'  # Replace title
     }
     render_template('quizzes/quiz_edit.html', **context)
+
+
+@app.route('/quizzes/<int:pk>/')  # primary key (Первичный ключ)
+def quiz_info(pk):
+    context = {
+        'title': 'QUIZ_NAME',  # Replace with the name of quiz
+    }
+    return render_template('quizzes/quiz_info.html', **context)
+
+
+@app.route('/quizzes/')
+def quizzes_list():
+    context = {
+        'title': 'List of Quizzes',
+    }
+    return render_template('quizzes/quizzes_lis.html', **context)
 
 
 @app.route('/quizzes/<int:pk>/passing/<int:question_num>')
